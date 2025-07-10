8727f17903ff50cadc2f766c2d0c8543eb32663f
Nima <Nimakhazayi7@gmail.com>
2025-07-10 13:10:37 +0330

    final project from cs50p v1.5

--- features/texteditor.py
+++ features/texteditor.py
@@ -1,12 +1,14 @@
 import curses
+import pyfiglet
 
 class TextEditor:
 
     def __init__(self):
         self.stdscr = curses.initscr()
         self.text = ""
-        self.current_line = 0
+        self.current_line = 8
         self.cursor_position = 0
+        self.height, self.width = self.stdscr.getmaxyx()
 
     def run(self):
         """Run a cell in terminal for displaying the editor"""
@@ -14,14 +16,16 @@ class TextEditor:
         self.stdscr.refresh()
 
         while True:
-            
-            self.stdscr.refresh()
 
+            message = pyfiglet.figlet_format("TextEditor", font="slant")
+            self.stdscr.addstr(0, 1, message)
+            self.stdscr.addstr(6, 2, "     ---------------------------      ")
+            self.movement()
             key = self.stdscr.getch()
             if self.handle_input(key):
                 break # Exit the editor
 
-            self.display_text()
+            self.stdscr.refresh()
         
         self.save_file()
 
@@ -38,6 +42,7 @@ class TextEditor:
         
         elif key == 127 or key == curses.KEY_BACKSPACE:
             self.positioning(key)
+            self.display_text()
 
         elif key == 10 or key == curses.KEY_ENTER:
             self.positioning(key)
@@ -50,42 +55,37 @@ class TextEditor:
         pass
 
     def positioning(self, key=None):
-
-        height, width = self.stdscr.getmaxyx()
-        position = f"Row: {self.current_line + 1},   Col: {self.cursor_position + 1}"
-        self.stdscr.addstr(height - 1, width // 2 - 15, position, curses.A_BOLD)
-
+        
         if key in (10, curses.KEY_ENTER):
-            self.cursor_position = 0
             self.current_line += 1
+            self.cursor_position = 0
         
         elif key in (127, curses.KEY_BACKSPACE):
-            if self.cursor_position > 0:
-                self.cursor_position -= 1
-                self.text.replace(self.text[-1], "")
-                
-                # Clear the last character from the screen
-                self.stdscr.addstr(self.current_line, self.cursor_position, ' ')
-                
-                # Check if we are at the start of a line
-                if self.cursor_position == 0 and self.current_line > 0:
-                    self.current_line -= 1
-                    self.cursor_position = len(self.text)  # Move cursor to the end of the previous line
+            pass
 
         else:
-
+            self.cursor_position += 1
             self.text += chr(key)
+            self.display_text()
+            
+        # Clear previous position display
+        self.stdscr.move(self.height - 1, 0)
+        self.stdscr.clrtoeol()
         
-            if self.cursor_position >= width:
-                self.cursor_position = 0
-                self.current_line += 1
+        # Draw new position
+        position = f"Row: {self.current_line + 1}, Col: {self.cursor_position + 1}"
+        self.stdscr.addstr(self.height - 1, self.width // 2 - len(position) // 2, position, curses.A_BOLD)
+        self.stdscr.refresh()
 
-            else:
-                self.cursor_position += 1
+        self.movement()
 
+        
+    def movement(self):
+        if self.cursor_position + 2 == self.width:
+            self.positioning(curses.KEY_ENTER)
 
-        self.stdscr.move(self.current_line, self.cursor_position)
+        self.stdscr.move(self.current_line, self.cursor_position + 1)
 
-
+        
 
 
