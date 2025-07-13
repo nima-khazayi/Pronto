206e5a3176f421ffd0b0ae541542cc375d6170b7
Nima <Nimakhazayi7@gmail.com>
2025-07-13 12:14:17 +0330

    final project from cs50p v1.5.6

--- features/texteditor.py
+++ features/texteditor.py
@@ -9,7 +9,7 @@ class TextEditor:
         self.text = ""
         self.current_line = 8
         self.cursor_position = 0
-        self.length = [0]
+        self.length = [""]
         self.height, self.width = self.stdscr.getmaxyx()
 
     def run(self):
@@ -31,8 +31,9 @@ class TextEditor:
 
     def display_text(self):
 
-        for char in (self.text):
-            self.stdscr.addstr(self.current_line, self.cursor_position, char)
+        for i in range(len(self.length)):
+            for char in (self.length[i]):
+                self.stdscr.addstr(self.current_line, self.cursor_position, char)
 
 
     def handle_input(self, key):
@@ -43,7 +44,7 @@ class TextEditor:
         elif key == 127 or key == curses.KEY_BACKSPACE:
             if self.cursor_position != 0:
                 self.stdscr.addstr(self.current_line, self.cursor_position, " ")
-                self.text = self.text[:-1]
+                self.length[self.current_line - 8] = self.length[self.current_line - 8][:-1]
                 self.positioning(key)
                 self.stdscr.refresh()
 
@@ -72,11 +73,10 @@ class TextEditor:
             if self.cursor_position + 2 == self.width:
                 self.enter()
 
-            self.text += chr(key)
+            self.length[self.current_line - 8] += chr(key)
             self.cursor_position += 1
             self.display_text()
-            self.length.append(self.cursor_position)
-            self.length.pop(-2)
+
             
         # Clear previous position display
         self.stdscr.move(self.height - 1, 0)
@@ -97,14 +97,14 @@ class TextEditor:
         if key == curses.KEY_UP:
             if self.current_line > 8:
                 self.current_line -= 1
-                prev_line_len = self.length[self.current_line - 8]
+                prev_line_len = len(self.length[self.current_line - 8])
                 self.cursor_position = min(self.cursor_position, prev_line_len)
                 self.movement()
 
         elif key == curses.KEY_DOWN:
             if self.current_line - 8 < len(self.length) - 1:
                 self.current_line += 1
-                next_line_len = self.length[self.current_line - 8]
+                next_line_len = len(self.length[self.current_line - 8])
                 self.cursor_position = min(self.cursor_position, next_line_len)
                 self.movement()
 
@@ -114,11 +114,11 @@ class TextEditor:
                 self.movement()
             elif self.current_line > 8:
                 self.current_line -= 1
-                self.cursor_position = self.length[self.current_line - 8]
+                self.cursor_position = len(self.length[self.current_line - 8])
                 self.movement()
 
         elif key == curses.KEY_RIGHT:
-            line_len = self.length[self.current_line - 8]
+            line_len = len(self.length[self.current_line - 8])
             if self.cursor_position < line_len:
                 self.cursor_position += 1
                 self.movement()
@@ -131,29 +131,35 @@ class TextEditor:
 
     def remove(self):
         if self.cursor_position == 0 and self.current_line == 8:
-            self.length.pop()
-            self.length.append(0)
+            pass
         
         elif self.cursor_position == 0:
+            if len(self.length[self.current_line - 8]) == 0:
+                self.length.pop(self.current_line - 8)
             self.current_line -= 1
-            self.cursor_position = self.length[self.current_line - 8]
-            self.length.pop()
+            self.cursor_position = len(self.length[self.current_line - 8])
             self.movement()
             
         else:
             self.cursor_position -= 1
             self.movement()
-            self.length[self.current_line - 8] = self.length[self.current_line - 8] - 1
+            self.length[self.current_line - 8] = self.length[self.current_line - 8][:-1]
+
+            
 
     def enter(self):
-        self.text += "\n"
-        self.length.append(0)
+        self.length[self.current_line - 8] += "\n"
+        self.length.append("")
         self.current_line += 1
         self.cursor_position = 0
 
 
     def save_file(self):
         file = open("file.txt", "w")
+        for i in range(len(self.length)):
+            for char in self.length[i]:
+                self.text += char
+
         file.write(self.text)
 
         # Initialize color support
